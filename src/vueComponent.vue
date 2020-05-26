1edd2529dca38681cc5799f84911bce0f89546c5
corpglory-dev <dev@corpglory.com>
2020-05-26 18:39:50 +0300

    events

--- src/vueComponent.vue
+++ src/vueComponent.vue
@@ -4,11 +4,13 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue, Prop, Watch } from 'vue-property-decorator';
-
 import { ChartwerkBase } from '../dist/index.js';
 
 import { Margin, TimeSerie, Options, TickOrientation, TimeFormat } from './types';
+
+import { Component, Vue, Prop, Watch } from 'vue-property-decorator';
+
+import * as _ from 'lodash';
 
 @Component
 export default class VueChartwerkBase extends Vue {
@@ -25,8 +27,47 @@ export default class VueChartwerkBase extends Vue {
   }
 
   renderBase(): void {
+    this.appendEvents();
     //@ts-ignore
     new ChartwerkBase(document.getElementById('vue-chartwerk-base'), this.series, this.options);
+  }
+
+  appendEvents(): void {
+    if(_.has(this.$listeners, '$listeners.zoomIn')) {
+      this.options.eventsCallbacks.zoomIn = this.zoomIn;
+    }
+    if(_.has(this.$listeners, '$listeners.zoomOut')) {
+      this.options.eventsCallbacks.zoomOut = this.zoomOut;
+    }
+    if(_.has(this.$listeners, '$listeners.mouseMove')) {
+      this.options.eventsCallbacks.mouseMove = this.mouseMove;
+    }
+    if(_.has(this.$listeners, '$listeners.mouseOut')) {
+      this.options.eventsCallbacks.mouseOut = this.mouseOut;
+    }
+    if(_.has(this.$listeners, '$listeners.onLegendClick')) {
+      this.options.eventsCallbacks.onLegendClick = this.onLegendClick;
+    }
+  }
+
+  zoomIn(): void {
+    this.$emit('zoomIn');
+  }
+
+  zoomOut(): void {
+    this.$emit('zoomOut');
+  }
+
+  mouseMove(): void {
+    this.$emit('mouseMove');
+  }
+
+  mouseOut(): void {
+    this.$emit('mouseOut');
+  }
+
+  onLegendClick(): void {
+    this.$emit('onLegendClick');
   }
 }
 </script>
